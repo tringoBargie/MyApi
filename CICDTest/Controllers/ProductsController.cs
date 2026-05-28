411bb4b4e83bd2a2ee66b2dfa88e44bf21d54645
tringoBargie <motherlife075@gmail.com>
2026-05-29 01:01:52 +0300

    Initial commit - Products API

---
+++ CICDTest/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CICDTest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private static List<string> _products = new()
+        {
+            "Laptop", "Mouse", "Keyboard"
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_products);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string product)
+        {
+            _products.Add(product);
+            return Ok($"Added {product}");
+        }
+
+    }
+}
