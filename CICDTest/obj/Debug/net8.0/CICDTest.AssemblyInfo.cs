b9890b82248e7a0c48724141a0aa7a3f20ed77fa
tringoBargie <motherlife075@gmail.com>
2026-05-29 01:57:01 +0300

    Add GitHub Actions CI/CD pipeline

--- CICDTest/obj/Debug/net8.0/CICDTest.AssemblyInfo.cs
+++ CICDTest/obj/Debug/net8.0/CICDTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CICDTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411bb4b4e83bd2a2ee66b2dfa88e44bf21d54645")]
 [assembly: System.Reflection.AssemblyProductAttribute("CICDTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CICDTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
