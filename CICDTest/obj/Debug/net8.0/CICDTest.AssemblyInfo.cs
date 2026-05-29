21cfedd004a33edcea6f7167d9128fb6cdba90f6
tringoBargie <motherlife075@gmail.com>
2026-05-29 10:55:49 +0300

    file stracture changed

--- CICDTest/obj/Debug/net8.0/CICDTest.AssemblyInfo.cs
+++ CICDTest/obj/Debug/net8.0/CICDTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CICDTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff681dc519445983ecce39e603d1c50254f6932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93df559a4e33b53a2b752e064dd1972b69bd53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CICDTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CICDTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
